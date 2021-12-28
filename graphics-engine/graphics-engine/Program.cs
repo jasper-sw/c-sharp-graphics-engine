9c2c6ff5f1d2d4bbb7db66e627324d30b0fb6110
Jasper Swensen <jasperswensen@gmail.com>
2021-12-27 21:42:48 -0700

    got the window working

---
+++ graphics-engine/graphics-engine/Program.cs
@@ -0,0 +1,14 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+
+namespace glob {
+    public class Game : GameWindow {
+        public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) { }
+
+        public static void Main() {
+        Game game = new Game(800, 600, "LearnOpenTK");
+        game.Run(60.0);
+        }
+
+    }
+}   
