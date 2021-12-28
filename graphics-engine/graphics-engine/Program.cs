d0daa5b1393b9cc33d1e91f93a39f9a6106d74d9
Jasper Swensen <jasperswensen@gmail.com>
2021-12-27 21:58:06 -0700

    change namespace

--- graphics-engine/graphics-engine/Program.cs
+++ graphics-engine/graphics-engine/Program.cs
@@ -1,7 +1,7 @@
 ﻿using OpenTK;
 using OpenTK.Graphics;
 
-namespace glob {
+namespace Window {
     public class Game : GameWindow {
         public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) { }
 
